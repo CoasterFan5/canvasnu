ac813c56f89f2ffaf18be3ef259a57052495b4c8
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-02-20 15:16:58 -0500

    Navbar

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,5 +1,40 @@
 <script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="wrap">
+	{@render children()}
+
+	<Navbar />
+</div>
+
+<style lang="scss">
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		background: var(--background);
+		color: var(--text);
+
+		--text: rgb(241, 241, 241);
+		--text10: color-mix(in srgb, 10% var(--text), 90% transparent);
+		--background: rgb(36, 31, 24);
+		--primary: rgb(251, 135, 2);
+		--primary10: color-mix(in srgb, 10% var(--primary), 90% transparent);
+		--secondary: rgb(0, 0, 0);
+
+		--accent: rgb(80, 80, 80);
+		--accent75: color-mix(in srgb, 75% var(--accent), 25% transparent);
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+	}
+
+	.wrap {
+		width: 100%;
+		height: 100vh;
+		overflow-y: auto;
+	}
+</style>
