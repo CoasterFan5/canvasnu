04dac0ea29184f1f49bfccdf8c54bec401c0dd8d
Dylan <56899612+CoasterFan5@users.noreply.github.com>
2025-08-27 10:10:26 -0400

    new assignments view

--- apps/web/src/routes/app/courses/[id]/assignments/+page.svelte
+++ apps/web/src/routes/app/courses/[id]/assignments/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import dayjs from 'dayjs';
 	import type { PageData } from './$types';
 
 	const {
@@ -11,21 +12,29 @@
 <div class="wrap">
 	<div class="left">
 		<div class="upcomingAssignments">
-			<h4>Upcoming</h4>
+			<span class="header"><h4>Upcoming</h4></span>
 			{#each data.upComingAssignments as assignment}
 				<a
 					class="assignment"
 					target="_blank"
 					href="https://{data.canvasDomain}{assignment.externalUrl}"
 				>
-					{assignment.name}</a
-				>
+					<div>{assignment.name}</div>
+					<div>{dayjs(assignment.dueDate).format('MMM D')}</div>
+				</a>
 			{/each}
 		</div>
-		<div class="allAssignments">
-			<h4>All Assignments</h4>
+		<div class="upcomingAssignments">
+			<span class="header"><h4>All Assignments</h4></span>
 			{#each data.allAssignments as assignment}
-				<a class="assignment" href="##">{assignment.name}</a>
+				<a
+					class="assignment"
+					target="_blank"
+					href="https://{data.canvasDomain}{assignment.externalUrl}"
+				>
+					<div>{assignment.name}</div>
+					<div>{dayjs(assignment.dueDate).format('MMM D')}</div>
+				</a>
 			{/each}
 		</div>
 	</div>
@@ -36,21 +45,41 @@
 	.wrap {
 		width: 100%;
 		height: 100%;
-		background: var(--secondary);
 		border-radius: 0.5rem;
-		padding: 1rem;
 	}
 
-	h4 {
-		font-weight: 500;
-		font-size: 1.1rem;
+	.left {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	.upcomingAssignments {
+		background: var(--secondary);
+		border-radius: 0.5rem;
+		overflow: hidden;
+	}
+
+	.header {
+		margin: 0;
+		padding: 0.5rem;
+		display: flex;
+		opacity: 0.8;
+		h4 {
+			margin: 0;
+			font-size: 0.8rem;
+			font-weight: 400;
+		}
 	}
 
 	.assignment {
+		border-top: 2px solid var(--background);
 		width: 100%;
 		display: flex;
-		background: var(--secondary);
+		align-items: center;
+		justify-content: space-between;
 		padding: 0.5rem;
-		border-radius: 0.5rem;
+		color: var(--text);
+		text-decoration: none;
 	}
 </style>
